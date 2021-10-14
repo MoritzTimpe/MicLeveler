467a0ed538aad3d155c91a8cab737c74d798d1e2
Moritz T <momo6601@web.de>
2021-10-14 17:24:24 +0200

    Including cfg
    Including cfg file for permanent setting

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from __future__ import print_function
 import os
 
+
 def calc(percent):
+    #wanted 45
     one_percent = 65536 / 100
-    return (one_percent * percent)
+    return (int(one_percent) * int(percent))
 
 def main():
-    percent = calc(int(input("Input wish percentage: ")))
-    os.system(f"nircmdc.exe loop 144000 250 setsysvolume {percent} default_record")
+    filesize = os.path.getsize("level.cfg")
+
+    if filesize == 0:
+        f = open('level.cfg', 'w+')
+        raw_lvl = input("Input wish percentage: ")
+        lvl = calc(raw_lvl)
+        f.write(str(raw_lvl))
+        f.close()
+    else:
+        f = open('level.cfg', 'r+')
+        lvl = calc(f.read())
+    os.system(f"nircmdc.exe 250 setsysvolume {lvl} default_record")
     #          nircmdc.exe loop /number of loops/ /time in ms to execute one loop/ setsysvolume /65536 == 100%/ /device/
 
 
